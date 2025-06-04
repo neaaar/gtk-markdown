151d8eaa5eb43645385ae58b773af4ede97f622f
Marco Farioli <99016665+neaaar@users.noreply.github.com>
2025-06-04 16:53:36 +0200

    Updated start-container.sh

--- start-container.sh
+++ start-container.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-# Nome dell'immagine Docker
+# Name of the Docker image
 IMAGE_NAME=gtk-markdown:latest
 
-# UID dell'utente (serve per Wayland e D-Bus)
+# User UID (needed for Wayland and D-Bus)
 USER_UID=$(id -u)
 WAYLAND_DISPLAY=${WAYLAND_DISPLAY:-wayland-0}
 XDG_RUNTIME_DIR=${XDG_RUNTIME_DIR:-/run/user/$USER_UID}
 
-# Esegui il container
+# Run the container
 docker run --rm -it \
   --env WAYLAND_DISPLAY=$WAYLAND_DISPLAY \
   --env XDG_RUNTIME_DIR=$XDG_RUNTIME_DIR \
@@ -17,4 +17,3 @@ docker run --rm -it \
   --network host \
   $IMAGE_NAME \
   bash
-
