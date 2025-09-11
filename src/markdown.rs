d50ee094e2b2dac2a8d6209aba1830a6dc5f17de
Marco Farioli <marco.farioli@outlook.it>
2025-09-11 18:34:36 +0200

    Add syntax highlighting using highlight.js

--- src/markdown.rs
+++ src/markdown.rs
@@ -1,10 +1,34 @@
 use pulldown_cmark::{html::push_html, Options, Parser};
+const CSS: &str = include_str!("../assets/github.min.css");
+const JS: &str = include_str!("../assets/highlight.min.js");
 
 pub fn to_html(input: &str) -> String {
-    let options = Options::all();
-    let parser = Parser::new_ext(input, options);
+    let parser = Parser::new_ext(input, Options::all());
 
-    let mut html = String::new();
-    push_html(&mut html, parser);
-    html
+    let mut html_content = String::new();
+    push_html(&mut html_content, parser);
+
+    format!(
+        r#"<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>GTK Markdown Preview</title>
+<style>{css}</style>
+<script>{js}</script>
+<script>hljs.highlightAll();</script>
+<style>
+    body {{ font-family: sans-serif; margin: 1rem; }}
+    pre {{ padding: 0.5rem; border-radius: 5px; }}
+    pre > code {{ display: block; }}
+</style>
+</head>
+<body>
+{html}
+</body>
+</html>"#,
+        css = CSS,
+        js = JS,
+        html = html_content
+    )
 }
